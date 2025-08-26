af838b4ac3c99c1682c1b4376754dae3d9a99663
Minh Vo (Monash University) <mvo1@m3t103.massive.org.au>
2025-08-26 23:59:23 +1000

    Move test folder outside repo and ignore it

--- llm-bn/bni/dll_smile/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-smoking_lung_cancer.dne"
-filePath2 = "outputs/test.dne"
-
-myNet = Net(netDir+filePath1)
-print("Re-opened network structure:")
-for node in myNet.nodes():
-		print(f"{node.name()} -> {[child.name() for child in node.children()]}")
