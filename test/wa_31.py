25d002b454611059e3ff7e24d17572113f8efcca
Minh Vo <66895221+minhvoio@users.noreply.github.com>
2025-09-19 14:20:06 +0000

    test: test wa_31

---
+++ test/wa_31.py
@@ -0,0 +1,10 @@
+FEEDBACKS = {
+  'miss_leader': "Team leader isn't mentioned (see page 6 - task 3.1 Assignment 1)",
+}
+
+QUESTIONS = [
+  """Is team leader mentioned in the Working Agreement table?
+  Yes/No
+  """,
+  
+]
